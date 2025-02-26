317fff3bf743200f9ae1b5ce817fbfecc670f3ac
akilsonestacio <akilson.vasconcelos@ufra.edu.br>
2025-02-26 19:37:23 +0000

    finalizado

--- MediaAritmética.c
+++ MediaAritmética.c
@@ -2,6 +2,22 @@
 
 int main(){
 
+    //Variáveis
+
+    float n1, n2, n3, media;
+
+    // Logica
+
+    printf("Digite a nota da 1ª avaliação: ");
+    scanf("%f", &n1);
+    printf("Digite a nora da 2ª avaliação: ");
+    scanf("%f", &n2);
+    printf("Digite a nota da 3ª avaliação: ");
+    scanf("%f", &n3);
+    media = (n1+n2+n3)/3;
+    printf("O valor da média é: %f", media);
+
+
     return 0;
     
 }
